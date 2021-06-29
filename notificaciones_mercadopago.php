0d5029ee6caf7be57c1629c8b15b021cf64edeaf
tmartty <tomasmartty@gmail.com>
2021-06-29 12:14:30 -0300

    + notifications webhook

--- notificaciones_mercadopago.php
+++ notificaciones_mercadopago.php
@@ -1,5 +1,17 @@
 <?php 
+    http_response_code(200);
 
+    $json = file_get_contents('php://input');
+    // Converts it into a PHP object 
+    $data = json_decode($json, true);
 
+    //Something to write to txt log
+    $log  = "Address: " . $_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a") . PHP_EOL .
+            "POST Data: " . $data . PHP_EOL .
+            "-------------------------" . PHP_EOL;
 
+    //Save string to log, use FILE_APPEND to append.
+    file_put_contents('./logs/log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+
+    return;
 ?>
